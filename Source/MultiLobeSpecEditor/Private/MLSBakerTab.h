0950bcc6ef682ffa7f1a3e4955ad058ba488d839
helmdubo <56157514+helmdubo@users.noreply.github.com>
2026-08-13 22:44:35 +0400

    Integrate AO assignment into baker UI

--- Source/MultiLobeSpecEditor/Private/MLSBakerTab.h
+++ Source/MultiLobeSpecEditor/Private/MLSBakerTab.h
@@ -1,47 +1,55 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "MLSBakerCore.h"
 #include "Widgets/SCompoundWidget.h"
+#include "MLSBakerCore.h"
 
+class STextBlock;
+class SVerticalBox;
 class UMaterialInstanceConstant;
 class UTexture2D;
-class STextBlock;
 
-/** P2 baker UI: RG normal -> Poisson height -> orthographic GTAO, then deterministic AO assignment. */
 class SMLSBakerTab : public SCompoundWidget
 {
 public:
 	SLATE_BEGIN_ARGS(SMLSBakerTab) {}
 	SLATE_END_ARGS()
 
-	void Construct(const FArguments& InArgs);
+	void Construct(const FArguments& Args);
 
 private:
-	FMLSBakeSettings Settings;
-	FString MasterFilter = TEXT("");
-	FString NormalSuffix = TEXT("_tex_n");
-	FString AOSuffix = TEXT("_tex_ao");
-	FString NormalParamNames = TEXT(""); // Empty = discover and sort normal parameters.
-	FString AOParamNames = TEXT("AO1,AO2,AO3");
-
-	TArray<TWeakObjectPtr<UTexture2D>> FoundNormals;
-	TArray<TWeakObjectPtr<UMaterialInstanceConstant>> FoundInstances;
-	TSharedPtr<STextBlock> ListTextWidget;
-	FString StatusText;
-
-	FReply OnFindFromSelection();
+	FReply OnGather();
 	FReply OnBake();
-	FReply OnAssign();
-	FReply OnAssignFromCurrentSelection();
-	FReply OnCycleReconstruction();
-	FReply OnCycleBoundary();
-
-	void GatherFromSelection();
-	int32 AssignBakedAOToGathered(FString& OutLog);
-	bool MatchesMasterFilter(class UMaterialInterface* Mat) const;
-	void RefreshFoundNormalList();
-
+	FReply OnCycleBakePreset();
+	void ApplyBakePreset(EMLSBakePreset Preset);
+	void MarkBakePresetCustom();
+	FText GetBakePresetText() const;
 	FText GetReconstructionText() const;
 	FText GetBoundaryText() const;
+	void RefreshFoundNormalList();
+	void RemoveNormalFromBakeList(TWeakObjectPtr<UTexture2D> Normal);
+	void GatherFromSelection(
+		TArray<TWeakObjectPtr<UTexture2D>>& OutNormals,
+		TArray<TWeakObjectPtr<UMaterialInstanceConstant>>& OutInstances,
+		FString& OutSummary) const;
+	bool AssignBakedAOToGathered(
+		const TSet<UTexture2D*>* AllowedNormals,
+		int32& OutAssignments,
+		int32& OutSkipped,
+		FString& OutDetails) const;
+	bool PassesMasterFilter(const UMaterialInstanceConstant* Instance) const;
+
+	FMLSBakeSettings Settings;
+	EMLSBakePreset BakePreset = EMLSBakePreset::StoneBrickMedium;
+	TArray<TWeakObjectPtr<UTexture2D>> FoundNormals;
+	TArray<TWeakObjectPtr<UMaterialInstanceConstant>> FoundInstances;
+	TSharedPtr<STextBlock> StatusText;
+	TSharedPtr<SVerticalBox> ListRowsWidget;
+
+	FString MasterFilter = TEXT("rendinst_vcolor_layered");
+	FString NormalParamNames;
+	FString AOParamNames = TEXT("AO1,AO2,AO3");
+	FString NormalSuffix = TEXT("_tex_n");
+	FString AOSuffix = TEXT("_tex_ao");
+	bool bAssignAfterBake = true;
 };
